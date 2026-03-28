8f0edf7503c9b74e6195b7bcdb6fed7e98d53e84
jubayer50 <aujubayer1@gmail.com>
2026-03-28 22:19:38 +0600

    navbar banner and footer section done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Banner from "./Components/Banner/Banner";
+import Footer from "./Components/Footer/Footer";
+import NavBar from "./Components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+
+      <Banner></Banner>
+
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
